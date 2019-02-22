6caadfd4224655c2a4c49737858322037eb06c30
Michael Borromeo <mbborromeo@gmail.com>
2019-02-22 13:08:52 +1100

    update src files

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,28 @@
-//  alert('hey there!')
+/* eslint-env jquery */
+/* global $ */
+
+(function () {
+  // use IIFE because I want to initially hide other card rows besides first row.
+  var $cardRows = $('.card-deck');
+  var $cardRowsLength = $cardRows.length;
+  var $cardRowCurrentIndex = 0;
+  var $buttonReadMoreCards = $('#btn_readMoreCards');
+
+  // hide all rows except first one
+  $cardRows.hide().eq(0).show();
+  // for (let i = 1; i < $cardRowsLength; i++) {
+  //   $cardRows.eq(i).hide();
+  // }
+
+  // when document has loaded
+  $(document).ready(function () {
+    // click handler for when 'Read More' button
+    $buttonReadMoreCards.on('click', function () {
+      // check not end of row
+      if ($cardRowCurrentIndex + 1 < $cardRowsLength) {
+        $cardRowCurrentIndex++;
+        $cardRows.eq($cardRowCurrentIndex).fadeIn('slow');
+      }
+    });
+  });
+}());
